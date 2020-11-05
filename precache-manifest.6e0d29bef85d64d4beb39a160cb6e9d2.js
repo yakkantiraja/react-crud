6d703e9fd1a09c331c2d67c3d1a6d5450cb0c977
yakkantiraja <career2raja@gmail.com>
2020-11-05 18:02:05 +0800

    Updates

---
+++ precache-manifest.6e0d29bef85d64d4beb39a160cb6e9d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ef64b61e98e53f9cf325a61fa356cb",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "9c12a3157126f4f04c84",
+    "url": "/react-crud/static/css/main.252f78bb.chunk.css"
+  },
+  {
+    "revision": "27da5ec459306b1a61bc",
+    "url": "/react-crud/static/js/2.002dd36f.chunk.js"
+  },
+  {
+    "revision": "9c12a3157126f4f04c84",
+    "url": "/react-crud/static/js/main.f68b5dde.chunk.js"
+  },
+  {
+    "revision": "89d3d13e5264c31dc1d4",
+    "url": "/react-crud/static/js/runtime~main.43666fd9.js"
+  }
+]);
